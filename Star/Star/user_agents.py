64dda5573420ff663e48abd8f0d94b5fa39edd9b
Chih-Hui <53479045+huihuiman@users.noreply.github.com>
2020-03-17 21:51:53 +0800

    Add files via upload

---
+++ Star/Star/user_agents.py
@@ -0,0 +1,10 @@
+user_agents = [
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.14 (KHTML, like Gecko) Version/6.0.1 Safari/536.26.14',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+        'Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.17',
+
+
+
+ ]
