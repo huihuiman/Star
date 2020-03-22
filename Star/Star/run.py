0aa0d82c76a7b7b250171ff049c3a64c30bbcaa7
Chih-Hui <53479045+huihuiman@users.noreply.github.com>
2020-03-22 09:35:48 +0800

    Update run.py

--- Star/Star/run.py
+++ Star/Star/run.py
@@ -1,6 +1,13 @@
 import time
 import os
+import subprocess
+import threading
 
 while True:
     os.system("scrapy crawl star")
     time.sleep(60)
+
+# def crawlByProcess():
+#     subprocess.check_output(['scrapy','crawl','star'])
+# timer = threading.Timer(3,crawlByProcess)
+# timer.start()
