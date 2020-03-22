094257f22f75a91f36be732c9503cdaea9a541bc
Chih-Hui <53479045+huihuiman@users.noreply.github.com>
2020-03-22 09:36:06 +0800

    Update user_agents.py

--- Star/Star/user_agents.py
+++ Star/Star/user_agents.py
@@ -4,7 +4,4 @@ user_agents = [
         'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
         'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
         'Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.17',
-
-
-
  ]
